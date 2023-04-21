1cfce12b72d5949610554ee3d1177d79ddf543ca
FedorowPavel <pavel.fedorov@exposit.com>
2023-04-21 13:29:46 +0300

    small refactoring

--- src/common/components/NotFoundPage.tsx
+++ src/common/components/NotFoundPage.tsx
@@ -1,8 +1,27 @@
+import {Box, Link, Typography} from "@mui/material";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import ErrorIcon from '@mui/icons-material/Error';
+
 export default function NotFoundPage() {
+  const navigate = useNavigate()
 
   return (
-    <div>
-      Not found
-    </div>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: '100px',
+      gap: '40px'
+    }}>
+      <ErrorIcon sx={{height: '100px', width: '100px'}}/>
+      <Typography variant={'h5'}>
+        This page does not exist
+      </Typography>
+      <Typography variant={'h5'}>
+        Go to <Link onClick={() => navigate('/')}>welcome page</Link>
+      </Typography>
+    </Box>
   );
 }
--- src/features/analitics/store/api.ts
+++ src/features/analitics/store/api.ts
@@ -3,9 +3,8 @@ import {processAnalyticData} from "../../../common/utils/analytics";
 
 export const analyticsApi = api.injectEndpoints({
   endpoints: (build) =>  ({
-    getReposAnalyticsData: build.query<any, string[]>({
+    getReposAnalyticsData: build.query<unknown, string[]>({
       providesTags: [ApiTags.ANALYTICS],
-      // @ts-ignore
       queryFn: async (repos, api, options, baseQuery) => {
         const results = await Promise.all(repos.map(repo => baseQuery({url: `repos/${repo}`})))
         const processedData = processAnalyticData(results)
